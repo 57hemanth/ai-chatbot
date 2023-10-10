1e67ba4f70dea37bd9417a3f733f50ae366aecb1
57hemanth <hemanthchowdary1234@gmail.com>
2023-10-10 22:32:51 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(morgan("dev"))
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-    credentials: true, origin: "https://chatbot-7mmv.onrender.com"
+    credentials: true, origin: "https://chatbot.hemu.dev"
 }))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(express.json())
